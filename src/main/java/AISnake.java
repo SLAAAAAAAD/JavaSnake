179b05380033846ae306864d05726e9eb7783664
alexander <alindman93@gmail.com>
2019-04-16 05:33:57 -0400

    variablalized some things in AISnake

--- src/main/java/AISnake.java
+++ src/main/java/AISnake.java
@@ -6,6 +6,14 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class AISnake extends Snake {
+
+    private double aiTargetLead = 50
+            , aiTargetWeight = 1
+            , aiAvoidDistance = 50
+            , aiAvoidWeight = 100
+            , aiSelfAvoidDistance = 50
+            , aiSelfAvoidWeight = 100;
+
     public AISnake(Point pos, Handler handler) {
         super(pos, handler);
         color = Color.red;
@@ -24,25 +32,24 @@ public class AISnake extends Snake {
         for (int i = 0; i < handler.getObjects().size(); i++) {
             DynamicObject tempObj = handler.getObjects().get(i);
             if (tempObj != this) {
-                Point temp = tempObj.getLead(50);
-                temp.setX(FuzzyMath.percentCloser(temp.getX(), pos.getX(), 0.5));
-                temp.setY(FuzzyMath.percentCloser(temp.getY(), pos.getY(), 0.5));
-                attractors.add(temp.getAttractionFrom(1.0, pos));
+                Point temp = tempObj.getLead(aiTargetLead);
+//                temp.setX(FuzzyMath.percentCloser(temp.getX(), pos.getX(), 0.5));
+//                temp.setY(FuzzyMath.percentCloser(temp.getY(), pos.getY(), 0.5));
+                attractors.add(temp.getAttractionFrom(aiTargetWeight, pos));
 
                 ArrayList<Line> tempAL = tempObj.getCollidables();
                 for (int j = 0; j < tempAL.size(); j++) {
                     Point temp2 = tempAL.get(j).getMidpoint();
-                    if (temp2.getRfrom(pos) < 50) {
-                        attractors.add(temp2.getOppositeFrom(pos).getAttractionFrom(100.0, pos));
+                    if (temp2.getRfrom(pos) < aiAvoidDistance) {
+                        attractors.add(temp2.getOppositeFrom(pos).getAttractionFrom(aiAvoidWeight, pos));
                     }
                 }
             } else {
                 ArrayList<Line> tempAL = tempObj.getCollidables();
                 for (int j = 0; j < tempAL.size() -50; j++) {
-//                    Point temp2 = tempAL.get(j).getMidpoint();
                     Point temp2 = tempAL.get(j).getMidpoint();
-                    if (temp2.getRfrom(getLead(0)) < 50) {
-                        attractors.add(temp2.getOppositeFrom(pos).getAttractionFrom(100.0, pos));
+                    if (temp2.getRfrom(getLead(0)) < aiSelfAvoidDistance) {
+                        attractors.add(temp2.getOppositeFrom(pos).getAttractionFrom(aiSelfAvoidWeight, pos));
                     }
                 }
             }
